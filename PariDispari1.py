4b0c6dadf3aeddb7a297100de25a94c38647cf49
kitri2 <dodo.dalpra@gmail.com>
2021-04-06 00:05:37 +0200

    Add files via upload

---
+++ PariDispari1.py
@@ -0,0 +1,15 @@
+def pari():
+    x = int(input("INPUT X"))
+    if x % 2 == 0:
+        print("x è pari")
+    else:
+        print("x è dispari")
+
+
+pari()
+
+restart = input("Riprovare?(s/n)")
+if restart == "s":
+    pari()
+else:
+    print ("pacco")
